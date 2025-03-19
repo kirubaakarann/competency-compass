bb469aecb7ff10c672bb95bf1f0d14d945e5ac24
Kiru (KIRUBAKARAN) <45659834+kirubaakarann@users.noreply.github.com>
2025-03-19 14:55:07 +0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-
+from .models import db, Department, Competency, CompetencyBehavior, JobRole, RoleCompetency, Employee, Assessment, AssessmentRating, DevelopmentPlan, DevelopmentAction, AIRecommendation
