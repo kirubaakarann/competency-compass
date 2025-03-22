5dafb22ad2041da0c19e6e7afdf60d61ff803c7b
Kiru (KIRUBAKARAN) <45659834+kirubaakarann@users.noreply.github.com>
2025-03-22 16:28:03 +0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -42,7 +42,6 @@ class Competency(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    category = db.Column(db.String(50), nullable=True)  # Add the category field
     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
