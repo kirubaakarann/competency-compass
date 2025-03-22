18d736bc2066307b9b0b195dd4a75adf04efd910
Kiru (KIRUBAKARAN) <45659834+kirubaakarann@users.noreply.github.com>
2025-03-22 16:03:00 +0400

    Update job_roles.py

--- routes/job_roles.py
+++ routes/job_roles.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from models.models import JobRole, Department, Competency, RoleCompetency, Employee, db
+from models import JobRole, Competency, RoleCompetency, db
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy import func
 
 job_roles = Blueprint('job_roles', __name__)
 
@@ -12,40 +11,40 @@ def index():
 
 @job_roles.route('/create')
 def create():
-    departments = Department.query.all()
     competencies = Competency.query.all()
-    return render_template('job_roles/create.html', departments=departments, competencies=competencies)
+    return render_template('job_roles/create.html', competencies=competencies)
 
 @job_roles.route('/store', methods=['POST'])
 def store():
     try:
         title = request.form['title']
-        description = request.form['description']
-        department_id = request.form['department_id']
+        description = request.form.get('description', '')
         
         # Create new job role
         job_role = JobRole(
             title=title,
-            description=description,
-            department_id=department_id
+            description=description
         )
         
         db.session.add(job_role)
         db.session.flush()  # Get the ID of the new job role
         
-        # Add competencies
+        # Process selected competencies
         competency_ids = request.form.getlist('competency_ids[]')
-        levels = request.form.getlist('levels[]')
-        weights = request.form.getlist('weights[]')
         
-        for i in range(len(competency_ids)):
-            role_competency = RoleCompetency(
-                job_role_id=job_role.id,
-                competency_id=competency_ids[i],
-                required_level=levels[i],
-                weight=weights[i]
-            )
-            db.session.add(role_competency)
+        if competency_ids:
+            for comp_id in competency_ids:
+                # Get the required level for this competency
+                required_level = request.form.get(f'required_levels[{comp_id}]', 3)
+                
+                # Create role competency mapping
+                role_competency = RoleCompetency(
+                    job_role_id=job_role.id,
+                    competency_id=int(comp_id),
+                    required_level=int(required_level)
+                )
+                
+                db.session.add(role_competency)
         
         db.session.commit()
         flash('Job role created successfully!', 'success')
@@ -59,47 +58,22 @@ def store():
 @job_roles.route('/<int:id>')
 def view(id):
     job_role = JobRole.query.get_or_404(id)
-    role_competencies = RoleCompetency.query.filter_by(job_role_id=id).all()
-    
-    # Get employees in this role with their average competency level
-    employees = db.session.query(
-        Employee,
-        func.avg(db.case(
-            (db.and_(
-                RoleCompetency.competency_id == Employee.competency_ratings.c.competency_id,
-                RoleCompetency.job_role_id == id
-            ), Employee.competency_ratings.c.rating),
-            else_=0
-        )).label('avg_competency')
-    ).filter(
-        Employee.job_role_id == id
-    ).group_by(
-        Employee.id
-    ).all()
-    
-    # Create a list of employee objects with avg_competency attribute
-    employees_with_avg = []
-    for emp, avg in employees:
-        emp.avg_competency = avg or 0
-        employees_with_avg.append(emp)
-    
-    return render_template('job_roles/view.html', 
-                           job_role=job_role, 
-                           role_competencies=role_competencies, 
-                           employees=employees_with_avg)
+    return render_template('job_roles/view.html', job_role=job_role)
 
 @job_roles.route('/<int:id>/edit')
 def edit(id):
     job_role = JobRole.query.get_or_404(id)
-    departments = Department.query.all()
     competencies = Competency.query.all()
-    role_competencies = RoleCompetency.query.filter_by(job_role_id=id).all()
+    
+    # Create a dictionary of existing role competencies for easier access in template
+    role_competencies_dict = {}
+    for rc in job_role.role_competencies:
+        role_competencies_dict[rc.competency_id] = rc
     
     return render_template('job_roles/edit.html', 
-                           job_role=job_role, 
-                           departments=departments, 
-                           competencies=competencies, 
-                           role_competencies=role_competencies)
+                          job_role=job_role, 
+                          competencies=competencies,
+                          role_competencies_dict=role_competencies_dict)
 
 @job_roles.route('/<int:id>/update', methods=['POST'])
 def update(id):
@@ -107,29 +81,31 @@ def update(id):
         job_role = JobRole.query.get_or_404(id)
         
         job_role.title = request.form['title']
-        job_role.description = request.form['description']
-        job_role.department_id = request.form['department_id']
+        job_role.description = request.form.get('description', '')
         
-        # Delete existing role competencies
+        # Remove existing role competencies
         RoleCompetency.query.filter_by(job_role_id=id).delete()
         
-        # Add updated competencies
+        # Process selected competencies
         competency_ids = request.form.getlist('competency_ids[]')
-        levels = request.form.getlist('levels[]')
-        weights = request.form.getlist('weights[]')
         
-        for i in range(len(competency_ids)):
-            role_competency = RoleCompetency(
-                job_role_id=job_role.id,
-                competency_id=competency_ids[i],
-                required_level=levels[i],
-                weight=weights[i]
-            )
-            db.session.add(role_competency)
+        if competency_ids:
+            for comp_id in competency_ids:
+                # Get the required level for this competency
+                required_level = request.form.get(f'required_levels[{comp_id}]', 3)
+                
+                # Create new role competency mapping
+                role_competency = RoleCompetency(
+                    job_role_id=job_role.id,
+                    competency_id=int(comp_id),
+                    required_level=int(required_level)
+                )
+                
+                db.session.add(role_competency)
         
         db.session.commit()
         flash('Job role updated successfully!', 'success')
-        return redirect(url_for('job_roles.view', id=job_role.id))
+        return redirect(url_for('job_roles.view', id=id))
     
     except SQLAlchemyError as e:
         db.session.rollback()
@@ -141,10 +117,9 @@ def delete(id):
     try:
         job_role = JobRole.query.get_or_404(id)
         
-        # Check if there are employees in this role
-        employees = Employee.query.filter_by(job_role_id=id).count()
-        if employees > 0:
-            flash(f'Cannot delete job role. There are {employees} employees assigned to this role.', 'error')
+        # Check if this job role is assigned to any employees
+        if job_role.employees:
+            flash('Cannot delete job role. It is assigned to one or more employees.', 'error')
             return redirect(url_for('job_roles.view', id=id))
         
         # Delete role competencies first
